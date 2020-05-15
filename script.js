7aada807af22ba3c8b2c1308f3f6fe3d8ba55075
Afua <afrimpong.15@gmail.com>
2020-05-15 17:58:43 -0400

    Converted xhr to fetch

--- script.js
+++ script.js
@@ -3,36 +3,26 @@
 
 // API Call
 // Get products to display from Makeup API
-const getMakeupPromise = (url) => {
-    // Creates new object
-    return new Promise ((resolve, reject) => {
-        const request = new XMLHttpRequest();
 
-        request.open('GET', url)
-        // Initializes new request as a GET and takes a url
-    
-        request.onload = function() {
-    
-            if(request.status === 200) {
-                resolve(JSON.parse(request.responseText));
+const makeupPromise = () => {
+    fetch("https://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeliner&product_category=liquid&price_greater_than=5&price_less_than=20")
 
-                // Data that comes back form a server is usually a string so we need to turn it into an JSON object we can pull info from
-            } else {
-    
-                reject('Request failed.  Returned status of ' + xhr.status);
-            }
-        }
+    .then( (res) => {
 
-        request.send()
-
+        return res.json()
     })
 
-    // Send the info to the server
+    .then ((data) => {
+        console.log(data)
+    })
+
+    .catch( (err) => {
+        console.log("No data to display")
+    })
 }
 
-getMakeupPromise("https://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeliner&product_category=liquid&price_greater_than=5&price_less_than=20").then((res) => {
-    console.log(res)
-})
+makeupPromise()
+
 
 
 // Create scroll event for button in hero
