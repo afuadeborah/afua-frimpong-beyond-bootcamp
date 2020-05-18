07af2981dfdb4710f2fb777abfbb5e81bc0ee2e1
Afua <afrimpong.15@gmail.com>
2020-05-17 23:09:36 -0400

    Data printing from API

--- script.js
+++ script.js
@@ -5,15 +5,28 @@
 // Get products to display from Makeup API
 
 const makeupPromise = () => {
+
     fetch("https://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeliner&product_category=liquid&price_greater_than=5&price_less_than=20")
 
     .then( (res) => {
-
         return res.json()
     })
 
     .then ((data) => {
         console.log(data)
+        // return data
+
+
+        // data.forEach((item) => {
+
+        //     const makeup = item.name
+        //     console.log(makeup)
+        // })
+        // Function manipulating data gets called here
+        
+        getMakeup(data)
+
+
     })
 
     .catch( (err) => {
@@ -23,8 +36,34 @@ const makeupPromise = () => {
 
 makeupPromise()
 
+const getMakeup = (data) => {
+
+    data.forEach((item) => {
+
+        const makeup = item.name
+        console.log(makeup)
+    })
+}
+
+
+
+
+
 
 
 // Create scroll event for button in hero
 // Create hover event for cart
 
+// <div class="image-container">
+
+// <img src="${item.image_link}" alt="${item.name}">
+
+// </div>
+
+// <div class="product-text">
+// <h3>
+//     <a href="${item.name}"></a>
+// </h3>
+// <p class="product-price">${item.price}</p>
+// <p class="product-brand">${item.brand}</p>
+// </div>
